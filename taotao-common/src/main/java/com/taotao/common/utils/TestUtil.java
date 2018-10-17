414988007c8df32b0710df1f585f8bc5585c3e25
sunmac <sunmac@192.168.1.103>
2018-10-17 22:34:22 +0800

    ....

--- taotao-common/src/main/java/com/taotao/common/utils/TestUtil.java
+++ taotao-common/src/main/java/com/taotao/common/utils/TestUtil.java
@@ -2,4 +2,12 @@ package com.taotao.common.utils;
 
 public class TestUtil {
     private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
