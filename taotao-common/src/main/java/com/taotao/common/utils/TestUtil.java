61974a3b4e4b7bc3d13a3053676f949a67d2a2cc
sunmac <sunmac@192.168.1.100>
2018-11-30 22:46:14 +0800

    测试提交

--- taotao-common/src/main/java/com/taotao/common/utils/TestUtil.java
+++ taotao-common/src/main/java/com/taotao/common/utils/TestUtil.java
@@ -2,7 +2,7 @@ package com.taotao.common.utils;
 
 public class TestUtil {
     private String name;
-
+    private String code;
 	public String getName() {
 		return name;
 	}
@@ -10,4 +10,13 @@ public class TestUtil {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
 }
